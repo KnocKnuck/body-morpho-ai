3b90594e19fe745cfd4a23387cd03a065ab9e027
Joseph HANI <53782645+KnocKnuck@users.noreply.github.com>
2020-12-17 12:57:53 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+import cv2
+import time
+import numpy as np
+from myWebCamVideoStream import WebCamVideoStream
+
+# Load Network File and Weights
+MODE = "MPI"
+protoFile = "model/pose_deploy_linevec_faster_4_stages.prototxt"
+weightsFile = "model/pose_iter_160000.caffemodel"
+nPoints = 15
+POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13] ]
+# Set parameters
+inWidth = 368
+inHeight = 368
+threshold = 0.1
+# Video from Live Camera using Thread
+cap = WebCamVideoStream(src=0).start()
+frame = cap.read() # Read current frame
+# Read network from Caffe
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+
+while cv2.waitKey(1) < 0:
+    frame = cap.read()
+    frameCopy = np.copy(frame)
+
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+
+    inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight),
+                              (0, 0, 0), swapRB=False, crop=False)
+    net.setInput(inpBlob)
+    output = net.forward()
+
+    H = output.shape[2]
+    W = output.shape[3]
+    # Empty list to store the detected keypoints
+    points = []
+
+    for i in range(nPoints):
+        # confidence map of corresponding body's part.
+        probMap = output[0, i, :, :]
+
+        # Find global maxima of the probMap.
+        minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+        
+        # Scale the point to fit on the original image
+        x = (frameWidth * point[0]) / W
+        y = (frameHeight * point[1]) / H
+
+        if prob > threshold : 
+            cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+            cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
+
+            # Add the point to the list if the probability is greater than the threshold
+            points.append((int(x), int(y)))
+        else :
+            points.append(None)
+
+    # Draw Skeleton
+    for pair in POSE_PAIRS:
+        partA = pair[0]
+        partB = pair[1]
+
+        if points[partA] and points[partB]:
+            cv2.line(frame, points[partA], points[partB], (0, 255, 255), 3, lineType=cv2.LINE_AA)
+            cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+            cv2.circle(frame, points[partB], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+    cv2.imshow('Skeleton', frame)
+
+cv2.destroyAllWindows()
